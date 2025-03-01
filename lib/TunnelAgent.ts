2ce4f71fb07bc5322ef73c1bcaafc0867dc05d52
Yu Li <contact+github@yuli.se>
2025-03-01 17:36:53 -0500

    tmp

--- lib/TunnelAgent.ts
+++ lib/TunnelAgent.ts
@@ -3,14 +3,13 @@ import { Agent } from 'http';
 import net, { Socket } from 'net';
 import { endOrDestroy } from './utils';
 
-const DEFAULT_MAX_SOCKETS = 10;
-
 // Implements an http.Agent interface to a pool of tunnel sockets
 // A tunnel socket is a connection _from_ a client that will
 // service http requests. This agent is usable wherever one can use an http.Agent
 interface TunnelAgentOptions {
   clientId: string;
-  maxTcpSockets?: number;
+  maxClientSockets: number;
+  maxTcpSockets: number;
 }
 
 interface ListenInfo {
@@ -27,16 +26,17 @@ class TunnelAgent extends Agent {
   private debug: Debug.Debugger;
   private connectedSockets: number;
   private maxTcpSockets: number;
+  private maxClientSockets: number;
   private server: net.Server;
   private started: boolean;
   private closed: boolean;
 
-  constructor(options: TunnelAgentOptions = { clientId: '' }) {
+  constructor(options: TunnelAgentOptions) {
     super({
       keepAlive: true,
       // only allow keepalive to hold on to one socket
       // this prevents it from holding on to all the sockets so they can be used for upgrades
-      maxFreeSockets: 1,
+      maxFreeSockets: options.maxClientSockets,
     });
 
     // sockets we can hand out via createConnection
@@ -50,7 +50,8 @@ class TunnelAgent extends Agent {
 
     // track maximum allowed sockets
     this.connectedSockets = 0;
-    this.maxTcpSockets = options.maxTcpSockets || DEFAULT_MAX_SOCKETS;
+    this.maxTcpSockets = options.maxTcpSockets;
+    this.maxClientSockets = options.maxClientSockets;
 
     // new tcp server to service requests for this client
     this.server = net.createServer();
@@ -114,16 +115,26 @@ class TunnelAgent extends Agent {
       return false;
     }
 
+    // When the client side sends FIN the socket, we must also send FIN to
+    // follow close the socket. This will trigger the 'close' event.
+    socket.once('end', () => {
+      this.debug('socket ended');
+      socket.end();
+    });
+
     socket.once('close', hadError => {
-      this.debug('closed socket (error: %s)', hadError);
       this.connectedSockets -= 1;
+
       // remove the socket from available list
       const idx = this.availableSockets.indexOf(socket);
       if (idx >= 0) {
         this.availableSockets.splice(idx, 1);
       }
 
-      this.debug('connected sockets: %s', this.connectedSockets);
+      this.debug(
+        `closed socket (error: ${hadError}). Socket counts: ${this.availableSockets.length} / ${this.connectedSockets}`,
+      );
+
       if (this.connectedSockets <= 0) {
         this.debug('all sockets disconnected');
         this.emit('offline');
@@ -132,9 +143,14 @@ class TunnelAgent extends Agent {
 
     // close will be emitted after this
     socket.once('error', err => {
+      this.debug('socket error %s', err);
       // we do not log these errors, sessions can drop from clients for many reasons
       // these are not actionable errors for our server
       endOrDestroy(socket);
+    });
+
+    socket.once('timeout', () => {
+      this.debug('socket timeout');
     });
 
     if (this.connectedSockets === 0) {
@@ -142,8 +158,6 @@ class TunnelAgent extends Agent {
     }
 
     this.connectedSockets += 1;
-    const { address, port } = socket.address() as net.AddressInfo;
-    this.debug('new connection from: %s:%s', address, port);
 
     // if there are queued callbacks, give this socket now and don't queue into available
     const fn = this.waitingCreateConn.shift();
@@ -152,11 +166,25 @@ class TunnelAgent extends Agent {
       setTimeout(() => {
         fn(null, socket);
       }, 0);
-      return;
+    } else {
+      // make socket available for those waiting on sockets
+      this.availableSockets.push(socket);
+      if (this.availableSockets.length > this.maxClientSockets) {
+        // Since the client would only open maxClientSockets sockets, if you
+        // reach here, it's likely the socket close event is not received by
+        // this server. Forcefully close and remove the oldest socket, which is
+        // the most likely one to have expired.
+        const removed = this.availableSockets.shift();
+        if (removed) {
+          endOrDestroy(removed);
+          this.connectedSockets -= 1;
+        }
+      }
     }
-
-    // make socket available for those waiting on sockets
-    this.availableSockets.push(socket);
+    const { address, port } = socket.address() as net.AddressInfo;
+    this.debug(
+      `New connection from: ${address}:${port}. Socket counts: ${this.availableSockets.length} / ${this.connectedSockets}`,
+    );
   }
 
   // fetch a socket from the available socket pool for the agent
@@ -182,7 +210,10 @@ class TunnelAgent extends Agent {
       return;
     }
 
-    this.debug('socket given');
+    this.debug(
+      `Socket given. Socket counts: ${this.availableSockets.length} / ${this.connectedSockets}`,
+    );
+
     cb(null, sock);
   }
 
